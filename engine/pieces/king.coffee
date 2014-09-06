5c8612aec31c36cfd730e028c795a5cc674c706d
RobertZK <technoguyrob@gmail.com>
2014-09-06 04:43:21 -0500

    king moves

--- engine/pieces/king.coffee
+++ engine/pieces/king.coffee
@@ -5,5 +5,52 @@ class King extends Piece
     super
     @type = 'king'
 
+  normalize_position: (old_x, old_y, x, y) ->
+    return { can_move: false } if y < 0
+    #if Math.abs(old_x - x) != 1 && Math.abs(old_y - y) != 1
+    #  throw "Only adjacent moves supported"
+
+    diff = x - old_x # This has to be precomputed before the % 24s
+    x = (x + 24) % 24
+    old_x = (old_x + 24) % 24
+    out = { can_move: true, crossed_circle: false }
+    if y > 5
+      offset =
+        if Math.abs(old_x - x) + Math.abs(old_y - y) == 2
+          14 # Like a bishop
+        else 
+          12 # Like a rook
+
+      if diff < 0
+        x = old_x + (24 - offset)
+        x %= 24
+      else
+        x = old_x + offset
+        x %= 24
+      y = 5
+      out.crossed_circle = true
+    
+    if has_piece = @board.has_piece_at(x, y)
+      same_color = @board.piece_at(x, y).color == @color
+      # We can only move onto a piece if it is of a different color.
+      out.can_move = !same_color
+    # If there is a piece, this is our final move in this direction.
+    out.final_move = has_piece
+    out.position = [x, y]
+    out
+
+  moves: ->
+    positions = []
+    tries = []
+    for dir in [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]]
+      prev_x = @x()
+      prev_y = @y()
+      [x, y] = [prev_x + dir[0], prev_y + dir[1]]
+      next_position = @normalize_position(prev_x, prev_y, x, y)
+      tries.push [x, y]
+      continue unless next_position.can_move
+      positions.push next_position.position
+    positions
+
 module.exports = King
 
--- engine/pieces/king.js
+++ engine/pieces/king.js
@@ -13,6 +13,61 @@
       this.type = 'king';
     }
 
+    King.prototype.normalize_position = function(old_x, old_y, x, y) {
+      var diff, has_piece, offset, out, same_color;
+      if (y < 0) {
+        return {
+          can_move: false
+        };
+      }
+      diff = x - old_x;
+      x = (x + 24) % 24;
+      old_x = (old_x + 24) % 24;
+      out = {
+        can_move: true,
+        crossed_circle: false
+      };
+      if (y > 5) {
+        offset = Math.abs(old_x - x) + Math.abs(old_y - y) === 2 ? 14 : 12;
+        if (diff < 0) {
+          x = old_x + (24 - offset);
+          x %= 24;
+        } else {
+          x = old_x + offset;
+          x %= 24;
+        }
+        y = 5;
+        out.crossed_circle = true;
+      }
+      if (has_piece = this.board.has_piece_at(x, y)) {
+        same_color = this.board.piece_at(x, y).color === this.color;
+        out.can_move = !same_color;
+      }
+      out.final_move = has_piece;
+      out.position = [x, y];
+      return out;
+    };
+
+    King.prototype.moves = function() {
+      var dir, next_position, positions, prev_x, prev_y, tries, x, y, _i, _len, _ref, _ref1;
+      positions = [];
+      tries = [];
+      _ref = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        dir = _ref[_i];
+        prev_x = this.x();
+        prev_y = this.y();
+        _ref1 = [prev_x + dir[0], prev_y + dir[1]], x = _ref1[0], y = _ref1[1];
+        next_position = this.normalize_position(prev_x, prev_y, x, y);
+        tries.push([x, y]);
+        if (!next_position.can_move) {
+          continue;
+        }
+        positions.push(next_position.position);
+      }
+      return positions;
+    };
+
     return King;
 
   })(Piece);
