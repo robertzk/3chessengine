7fd08b17a911bbbddc00e5f3566a8cc401c4287f
RobertZK <technoguyrob@gmail.com>
2014-09-06 22:56:24 -0500

    Finish octopus refactoring

--- engine/pieces/octopus.coffee
+++ engine/pieces/octopus.coffee
@@ -28,7 +28,7 @@
 #  - crossed_circle: Whether or not we have crossed the inner circle
 #     with this move.
 #  - position: The normalized x and y coordinates.
-normalize_position: (old_x, old_y, x, y) ->
+normalize_position = (old_x, old_y, x, y) ->
   return { can_move: false } if y < 0
   if Math.abs(old_x - x) != 1 && Math.abs(old_y - y) != 1
     throw "Only adjacent moves supported"
@@ -41,7 +41,7 @@ normalize_position: (old_x, old_y, x, y) ->
     offset =
       if Math.abs(old_x - x) + Math.abs(old_y - y) == 2
         14 # Like a bishop
-      else 
+      else
         12 # Like a rook
 
     if diff < 0
@@ -57,28 +57,40 @@ normalize_position: (old_x, old_y, x, y) ->
     same_color = @board.piece_at(x, y).color == @color
     # We can only move onto a piece if it is of a different color.
     out.can_move = !same_color
+
   # If there is a piece, this is our final move in this direction.
   out.final_move = has_piece
   out.position = [x, y]
   out
 
-moves: ->
+# Compute all diagonal or axial moves for a piece. If King, we can
+# set one_step = true so we only compute one step forward.
+# This function is intended to be used for Queen, Bishop, Rook, and King
+# as a method.
+moves = (diagonal, axial, one_step = false) -> ->
   positions = []
+  dirs = []
+  if axial
+    dirs.push x for x in [[-1, 0], [1, 0], [0, -1], [0, 1]]
+  if diagonal
+    dirs.push x for x in [[-1, -1], [-1, 1], [1, -1], [1, 1]]
+    
   tries = []
-  for dir in [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]]
+  for dir in dirs
     prev_x = @x()
     prev_y = @y()
     loop
       [x, y] = [prev_x + dir[0], prev_y + dir[1]]
-      next_position = @normalize_position(prev_x, prev_y, x, y)
+      next_position = normalize_position.call(@, prev_x, prev_y, x, y)
       tries.push [x, y]
       break unless next_position.can_move
       positions.push next_position.position
       # If the bishop crosses the inner circle,
       # we need to move "down" on the Y axis.
       dir[1] = -dir[1] if next_position.crossed_circle
-      break if next_position.final_move
+      break if next_position.final_move || one_step
       [prev_x, prev_y] = next_position.position
+
   positions
 
 module.exports = moves
--- engine/pieces/octopus.js
+++ engine/pieces/octopus.js
@@ -1,54 +1,74 @@
 (function() {
-  ({
-    normalize_position: function(old_x, old_y, x, y) {
-      var diff, has_piece, offset, out, same_color;
-      if (y < 0) {
-        return {
-          can_move: false
-        };
-      }
-      if (Math.abs(old_x - x) !== 1 && Math.abs(old_y - y) !== 1) {
-        throw "Only adjacent moves supported";
-      }
-      diff = x - old_x;
-      x = (x + 24) % 24;
-      old_x = (old_x + 24) % 24;
-      out = {
-        can_move: true,
-        crossed_circle: false
+  var moves, normalize_position;
+
+  normalize_position = function(old_x, old_y, x, y) {
+    var diff, has_piece, offset, out, same_color;
+    if (y < 0) {
+      return {
+        can_move: false
       };
-      if (y > 5) {
-        offset = Math.abs(old_x - x) + Math.abs(old_y - y) === 2 ? 14 : 12;
-        if (diff < 0) {
-          x = old_x + (24 - offset);
-          x %= 24;
-        } else {
-          x = old_x + offset;
-          x %= 24;
-        }
-        y = 5;
-        out.crossed_circle = true;
+    }
+    if (Math.abs(old_x - x) !== 1 && Math.abs(old_y - y) !== 1) {
+      throw "Only adjacent moves supported";
+    }
+    diff = x - old_x;
+    x = (x + 24) % 24;
+    old_x = (old_x + 24) % 24;
+    out = {
+      can_move: true,
+      crossed_circle: false
+    };
+    if (y > 5) {
+      offset = Math.abs(old_x - x) + Math.abs(old_y - y) === 2 ? 14 : 12;
+      if (diff < 0) {
+        x = old_x + (24 - offset);
+        x %= 24;
+      } else {
+        x = old_x + offset;
+        x %= 24;
       }
-      if (has_piece = this.board.has_piece_at(x, y)) {
-        same_color = this.board.piece_at(x, y).color === this.color;
-        out.can_move = !same_color;
-      }
-      out.final_move = has_piece;
-      out.position = [x, y];
-      return out;
-    },
-    moves: function() {
-      var dir, next_position, positions, prev_x, prev_y, tries, x, y, _i, _len, _ref, _ref1, _ref2;
+      y = 5;
+      out.crossed_circle = true;
+    }
+    if (has_piece = this.board.has_piece_at(x, y)) {
+      same_color = this.board.piece_at(x, y).color === this.color;
+      out.can_move = !same_color;
+    }
+    out.final_move = has_piece;
+    out.position = [x, y];
+    return out;
+  };
+
+  moves = function(diagonal, axial, one_step) {
+    if (one_step == null) {
+      one_step = false;
+    }
+    return function() {
+      var dir, dirs, next_position, positions, prev_x, prev_y, tries, x, y, _i, _j, _k, _len, _len1, _len2, _ref, _ref1, _ref2, _ref3;
       positions = [];
+      dirs = [];
+      if (axial) {
+        _ref = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          x = _ref[_i];
+          dirs.push(x);
+        }
+      }
+      if (diagonal) {
+        _ref1 = [[-1, -1], [-1, 1], [1, -1], [1, 1]];
+        for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+          x = _ref1[_j];
+          dirs.push(x);
+        }
+      }
       tries = [];
-      _ref = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]];
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        dir = _ref[_i];
+      for (_k = 0, _len2 = dirs.length; _k < _len2; _k++) {
+        dir = dirs[_k];
         prev_x = this.x();
         prev_y = this.y();
         while (true) {
-          _ref1 = [prev_x + dir[0], prev_y + dir[1]], x = _ref1[0], y = _ref1[1];
-          next_position = this.normalize_position(prev_x, prev_y, x, y);
+          _ref2 = [prev_x + dir[0], prev_y + dir[1]], x = _ref2[0], y = _ref2[1];
+          next_position = normalize_position.call(this, prev_x, prev_y, x, y);
           tries.push([x, y]);
           if (!next_position.can_move) {
             break;
@@ -57,15 +77,15 @@
           if (next_position.crossed_circle) {
             dir[1] = -dir[1];
           }
-          if (next_position.final_move) {
+          if (next_position.final_move || one_step) {
             break;
           }
-          _ref2 = next_position.position, prev_x = _ref2[0], prev_y = _ref2[1];
+          _ref3 = next_position.position, prev_x = _ref3[0], prev_y = _ref3[1];
         }
       }
       return positions;
-    }
-  });
+    };
+  };
 
   module.exports = moves;
 
